1efb7ebd3db1f2a7499c9e91e2a2f1f024ad6bec
brainuser5705 <codeuser5705@outlook.com>
2021-08-29 08:53:24 -0400

    made clue concatenator script

---
+++ script.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+link = 'https://www.youtube.com/watch?v='
+
+for i in range(1,4):
+    filename = "../solution/" + str(i) + ".txt"
+    try:
+        link += open(filename).read()
+    except FileNotFoundError:
+        link = 'https://www.youtube.com/watch?v=' # resetting link
+        print(filename + " cannot be found.")
+
+try:
+    webbrowser.open(link) 
+except:
+    print("ERROR: Unable to open link. Make sure that the video id is correct.")
